e29d64e60fc54f77849b0f0d6b6bac471b0057dc
Shrawan <xiaoennanjing@gmail.com>
2023-06-24 12:25:11 +0545

    final commit

---
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../../styles/styles";
+
+const Hero = () => {
+  return (
+    <div
+      className={`relative min-h-[50vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1528399783831-8318d62d10e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1500&q=80)",
+      }}
+    >
+      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
+        <h1
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#30154b] font-[600] capitalize`}
+        >
+          Welcome To <br /> Haki Store
+        </h1>
+        <br/>
+        <h3 className={`text-[20px] leading-[1.2] 500px:text-[60px] text-[#30154b] font-[600]`}>Discover, Explore and Choose</h3>
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#30154b]">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
+          assumenda? Quisquam itaque <br /> exercitationem labore vel, dolore
+          quidem asperiores, laudantium temporibus soluta optio consequatur{" "}
+          <br /> aliquam deserunt officia. Dolorum saepe nulla provident.
+        </p>
+        <Link to="/products" className="inline-block">
+            <div className={`${styles.button} mt-5`}>
+                 <span className="text-[#fff] font-[Poppins] text-[18px]">
+                    Shop Now
+                 </span>
+            </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
